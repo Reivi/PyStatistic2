8d8a1b99f6c40e6f1eb24cbf3dfaa74c6cec481a
User <vital.tretiakoff@yandex.ru>
2023-04-26 09:35:19 +0300

    Beta 1.2

--- main.py
+++ main.py
@@ -42,12 +42,18 @@ class ExampleApp(QtWidgets.QMainWindow, loadUi.Ui_MainWindow):
         self.window.showGrid(y=True)
         self.window.setMouseEnabled(x=False, y=False)
 
+        self.window_Pi = pg.plot()
+        self.Widget_Layout.addWidget(self.window_Pi)
+        self.window_Pi.setBackground('w')
+        self.window_Pi.showGrid(y=True)
+        self.window_Pi.setMouseEnabled(x=False, y=False)
+
         self.action_Excel.triggered.connect(self.Export_to_Exel)
-        self.import_Excel.triggered.connect(self.Import_to_Exel)
+
 
         self.Name_graf.setPlaceholderText('Название проекта')
 
-    def plot(self, axisX, axisY):
+    def plot(self, axisX, axisY,Pi):
         self.window.clear()
         self.window.setLabel('left', 'Количество')
         self.window.setLabel('bottom', 'Интервалы')
@@ -60,11 +66,24 @@ class ExampleApp(QtWidgets.QMainWindow, loadUi.Ui_MainWindow):
         self.window.addItem(pg.BarGraphItem(x=list(xdict.keys()), height=axisY, width=0.6, brush='g'))
         self.window.setTitle(self.Name_graf.text(), size="25pt", color="black")
 
-    def Export_to_Exel(self):
+        self.window_Pi.clear()
+        self.window_Pi.setLabel('left', 'Pi')
+        self.window_Pi.setLabel('bottom', 'Интервалы')
+        xax = self.window_Pi.getAxis('bottom')
+        xax.setTicks(ticks)
+        self.window_Pi.addItem(pg.BarGraphItem(x=list(xdict.keys()), height=Pi, width=0.6, brush='g'))
+        self.window_Pi.setTitle(self.Name_graf.text(), size="25pt", color="black")
 
+    def Export_to_Exel(self):
+        self.get_data()
         my_wb = openpyxl.Workbook()
         my_sheet = my_wb.active
         c1 = my_sheet.cell(row=1, column=1)
+        c1.value = "№ опыта"
+        bold_font = Font(bold=True)
+        c1.alignment = Alignment(horizontal='center')
+        c1.font = bold_font
+        c1 = my_sheet.cell(row=1, column=2)
         c1.value = "Данные"
         bold_font = Font(bold=True)
         c1.alignment = Alignment(horizontal='center')
@@ -72,56 +91,66 @@ class ExampleApp(QtWidgets.QMainWindow, loadUi.Ui_MainWindow):
         error_dialog = QtWidgets.QErrorMessage()
         try:
             for i in range(self.Table_input.rowCount()):
-                c1 = my_sheet.cell(row=i + 2, column=1)
+                c1 = my_sheet.cell(row=i + 2, column=2)
                 c1.value = float(self.Table_input.item(i, 0).text())
+                c1.alignment = Alignment(horizontal='center')
+                c1 = my_sheet.cell(row=i + 2, column=1)
+                c1.value = i + 1
                 c1.alignment = Alignment(horizontal='center')
         except:
             pass
-        c1 = my_sheet.cell(row=1, column=3)
+        c1 = my_sheet.cell(row=1, column=4)
         c1.value = "Интервал"
         c1.font = bold_font
-        c1 = my_sheet.cell(row=1, column=4)
+        c1 = my_sheet.cell(row=1, column=5)
         c1.value = "Количество"
         c1.font = bold_font
+        c1 = my_sheet.cell(row=1, column=6)
+        c1.value = "Pi"
+        c1.font = bold_font
+        c1.alignment = Alignment(horizontal='center')
 
         for i in range(7):
-            c1 = my_sheet.cell(row=i + 2, column=3)
+            c1 = my_sheet.cell(row=i + 2, column=4)
             try:
-                c1.value =self.Table_result.item(i, 0).text()
+                c1.value = self.Table_result.item(i, 0).text()
             except:
                 error_dialog.showMessage('Данные не обработаны!')
             c1.alignment = Alignment(horizontal='center')
-            c1 = my_sheet.cell(row=i + 2, column=4)
+            c1 = my_sheet.cell(row=i + 2, column=5)
             c1.value = float(self.Table_result.item(i, 1).text())
             c1.alignment = Alignment(horizontal='center')
+            c1 = my_sheet.cell(row=i + 2, column=6)
+            c1.value = float(self.Table_result.item(i, 2).text())
+            c1.alignment = Alignment(horizontal='center')
 
-        c1 = my_sheet.cell(row=1, column=7)
+        c1 = my_sheet.cell(row=1, column=9)
         c1.value = "Параметр"
         c1.font = bold_font
-        c1 = my_sheet.cell(row=1, column=6)
+        c1 = my_sheet.cell(row=1, column=8)
         c1.value = "Название"
         c1.font = bold_font
-        c1 = my_sheet.cell(row=2, column=6)
+        c1 = my_sheet.cell(row=2, column=8)
         c1.value = "Дисперсия"
         c1.font = bold_font
-        c1 = my_sheet.cell(row=3, column=6)
+        c1 = my_sheet.cell(row=3, column=8)
         c1.value = "Сред. квад. окл."
         c1.font = bold_font
-        c1 = my_sheet.cell(row=4, column=6)
+        c1 = my_sheet.cell(row=4, column=8)
         c1.value = "Среднее знач."
         c1.font = bold_font
-        c1 = my_sheet.cell(row=5, column=6)
+        c1 = my_sheet.cell(row=5, column=8)
         c1.value = "Коэф. вариац."
         c1.font = bold_font
-        c1 = my_sheet.cell(row=6, column=6)
+        c1 = my_sheet.cell(row=6, column=8)
         c1.value = "Мин."
         c1.font = bold_font
-        c1 = my_sheet.cell(row=7, column=6)
+        c1 = my_sheet.cell(row=7, column=8)
         c1.value = "Макс."
         c1.font = bold_font
 
         for i in range(6):
-            c1 = my_sheet.cell(row=i + 2, column=7)
+            c1 = my_sheet.cell(row=i + 2, column=9)
             c1.value = self.Table_statistic.item(i, 1).text()
             c1.alignment = Alignment(horizontal='center')
 
@@ -139,15 +168,37 @@ class ExampleApp(QtWidgets.QMainWindow, loadUi.Ui_MainWindow):
         chart1.height = 11
         chart1.title = self.Name_graf.text()
         # выберем 2 столбца с данными для оси `y`
-        data = Reference(my_sheet, min_col=4, max_col=4, min_row=1, max_row=8)
+        data = Reference(my_sheet, min_col=5, max_col=5, min_row=1, max_row=8)
         # теперь выберем категорию для оси `x`
-        categor = Reference(my_sheet, min_col=3, max_col=3, min_row=2, max_row=8)
+        categor = Reference(my_sheet, min_col=4, max_col=4, min_row=2, max_row=8)
         # добавляем данные в объект диаграммы
         chart1.add_data(data, titles_from_data=True)
         # установим метки на объект диаграммы
         chart1.set_categories(categor)
-        # добавим диаграмму на лист, в ячейку "A10"
-        my_sheet.add_chart(chart1, "J2")
+        my_sheet.add_chart(chart1, "K2")
+
+        chart2 = BarChart()
+        # установим тип - `вертикальные столбцы`
+        chart2.type = "col"
+        # установим стиль диаграммы (цветовая схема)
+        chart2.style = 8
+
+        chart2.y_axis.title = 'Pi'
+        chart2.y_axis.delete = False
+        chart2.x_axis.title = 'Интервалы'
+        chart2.x_axis.delete = False
+        chart2.width = 15
+        chart2.height = 11
+        chart2.title = self.Name_graf.text()
+        # выберем 2 столбца с данными для оси `y`
+        data = Reference(my_sheet, min_col=6, max_col=6, min_row=1, max_row=8)
+        # теперь выберем категорию для оси `x`
+        categor = Reference(my_sheet, min_col=4, max_col=4, min_row=2, max_row=8)
+        # добавляем данные в объект диаграммы
+        chart2.add_data(data, titles_from_data=True)
+        # установим метки на объект диаграммы
+        chart2.set_categories(categor)
+        my_sheet.add_chart(chart2, "K24")
 
         if self.Name_graf.text() != "":
             save_name = self.Name_graf.text() + ".xlsx"
@@ -165,12 +216,7 @@ class ExampleApp(QtWidgets.QMainWindow, loadUi.Ui_MainWindow):
         if fileName:
             my_wb.save(fileName)
 
-    def Import_to_Exel(self):
 
-        fileName, _ = QFileDialog.getOpenFileName(self, "Импорт из Excel", "",
-                                                  "Excel Files (*.xlsx);;All Files (*)")
-        if fileName:
-            print(fileName)
 
     def date_statistic_set(self, variance, sred_kvad_otkl, sr_snaz, koev_variazii, maxX, minX):
         self.Table_statistic.setRowCount(10)
@@ -178,11 +224,11 @@ class ExampleApp(QtWidgets.QMainWindow, loadUi.Ui_MainWindow):
         self.Table_statistic.setHorizontalHeaderLabels(["Параметр", "Значение"])
         self.Table_statistic.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.Table_statistic.setItem(0, 0, QTableWidgetItem("Дисперсия"))
-        self.Table_statistic.setItem(1, 0, QTableWidgetItem("Сред. квад. окл."))
-        self.Table_statistic.setItem(2, 0, QTableWidgetItem("Среднее знач."))
-        self.Table_statistic.setItem(3, 0, QTableWidgetItem("Коэф. вариац."))
-        self.Table_statistic.setItem(4, 0, QTableWidgetItem("Мин."))
-        self.Table_statistic.setItem(5, 0, QTableWidgetItem("Макс."))
+        self.Table_statistic.setItem(1, 0, QTableWidgetItem("Среднее квад. откл."))
+        self.Table_statistic.setItem(2, 0, QTableWidgetItem("Среднее значение"))
+        self.Table_statistic.setItem(3, 0, QTableWidgetItem("Коэффициент вариации"))
+        self.Table_statistic.setItem(4, 0, QTableWidgetItem("Минимальное"))
+        self.Table_statistic.setItem(5, 0, QTableWidgetItem("Максимальное"))
 
         self.Table_statistic.setItem(0, 1, QTableWidgetItem(str(f"{variance:.8}")))
         self.Table_statistic.setItem(1, 1, QTableWidgetItem(str(f"{sred_kvad_otkl:.8}")))
@@ -191,20 +237,22 @@ class ExampleApp(QtWidgets.QMainWindow, loadUi.Ui_MainWindow):
         self.Table_statistic.setItem(4, 1, QTableWidgetItem(str(f"{minX:.8}")))
         self.Table_statistic.setItem(5, 1, QTableWidgetItem(str(f"{maxX:.8}")))
 
-    def date_set(self, kol_interval, interval):
+    def date_set(self, kol_interval, interval, Pi):
+
         self.Table_result.setRowCount(kol_interval)
-        self.Table_result.setColumnCount(2)
-        self.Table_result.setHorizontalHeaderLabels(["Интервал", "Количество"])
+        self.Table_result.setColumnCount(3)
+        self.Table_result.setHorizontalHeaderLabels(["Интервал", "Количество", "Pi"])
+
         spam = [1 * 3 for i in range(kol_interval)]
         tatl = [1 * 3 for i in range(kol_interval)]
         for i in range(kol_interval):
             spam[i] = interval[i][2]
             tatl[i] = QTableWidgetItem(
-                str("{}-{}".format(f"{interval[i][0]:.3}", f"{interval[i][1]:.3}"))).text()
+                str("{}-{}".format(f"{interval[i][0]:.5}", f"{interval[i][1]:.5}"))).text()
             self.Table_result.setItem(i, 0, QTableWidgetItem(str(tatl[i])))
             self.Table_result.setItem(i, 1, QTableWidgetItem(str(interval[i][2])))
-
-        self.plot(tatl, spam)
+            self.Table_result.setItem(i, 2, QTableWidgetItem(str(f"{Pi[i]:.3}")))
+        self.plot(tatl, spam, Pi)
 
     def change(self):
         self.Table_input.setRowCount(int(self.Spin_input_table.text()))
@@ -260,9 +308,14 @@ class ExampleApp(QtWidgets.QMainWindow, loadUi.Ui_MainWindow):
                         pass
                 interval[b][2] = numbers_in_interval
                 numbers_in_interval = 0
-            self.date_set(kol_interval, interval)
-            self.date_statistic_set(variance, sred_kvad_otkl, sr_snaz, koev_variazii, maxX, minX)
 
+            Pi = [1 * 1 for i in range(kol_interval)]
+
+            for i in range(kol_interval):
+                Pi[i] = float((1 / len(Data)) * interval[i][2])
+
+            self.date_set(kol_interval, interval, Pi)
+            self.date_statistic_set(variance, sred_kvad_otkl, sr_snaz, koev_variazii, maxX, minX)
 
 
 app = QtWidgets.QApplication([])
